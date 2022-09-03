a454d7f907f84273b4406d36d1e767a451a4dc7c
kurehajime <xiidec@gmail.com>
2022-09-03 23:16:09 +0900

    Delete targetPointState.ts

--- src/states/targetPointState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-import { Point } from "../models/Point";
-
-export const TargetPointState = atom<Point>({
-    key: 'TargetPointState',
-    default: { x: 0, y: 0 }
-});
